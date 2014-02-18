947ec7f421aae33cebc620f3fea464a31c64b868
a1ultima <a1ultima@gmail.com>
2014-02-18 18:02:09 +0000

    meme_dataPrepper.py - V5 - LCR masking via Dustmasker

--- 2kb/scripts/meme_dataPrepper.py
+++ 2kb/scripts/meme_dataPrepper.py
@@ -1,8 +1,8 @@
-
+import subprocess
 
 # Need to ensure data is ready for MEME
 
-# HEADER MODE:
+# TEST MODE:
 # file_in_name    = '../data/meme_data/test/anopheles_gambiae_upstream_memeready_100.fasta'
 # file_out_name   = '../data/meme_data/test/anopheles_gambiae_upstream_memeready_independent.fasta'
 
@@ -10,11 +10,10 @@
 file_in_name    = '../data/meme_data/test/anopheles_gambiae_upstream.fasta'
 file_out_name   = '../data/meme_data/test/anopheles_gambiae_upstream_memeready_all.fasta'
 
-file_in     = open(file_in_name)                   # Open file for reading
-file_out    = open(file_out_name,'w')
-
+file_in  = open(file_in_name)        # Read file in order to establish filters that will later format the data ready for MEME
+file_out = open(file_out_name,'w')
 headers = {}
-print 'reading file...'
+print 'Establishing filters...'
 while True:
     header  = file_in.readline()
     sequence= file_in.readline()
@@ -30,32 +29,118 @@ while True:
     end         = utrCoord[1].split('-')[1]
     strand      = utrCoord[2]
     headers[geneId] = {'utrCoord':{'chromosome':chromosome,'start':start,'end':end,'strand':strand},'utrLength':utrLength,'utrType':utrType}
-
-    #  Can be a problem to try and distirbute out the overlapping sample_seqs <==...
-    # 
-    # Samples: 
-    #  s1             ----------------------
-    #  s2       --------------------
-    #  s3                             -----------
-    #  s4         -----------
-    #  s5             ------------------
-    # 
-    #       ...well then how can we possibly distribute out this overlapping region between correponding samples? 
-    #          ...there seems only one option: remove the intersect! => but then our data will be so sparse!
-    #
-    # After rm intersect:
-    #
-    #  s1        |   ----------------------|
-    #  s2      --|------------------       |
-    #  s3        |                    -----|------
-    #  s4        |-----------              |
-    #  s5        |   ------------------    |
-    # 
-    #           ^ Not much data left!!!     ^
-    #
-    #
 file_in.close()
 
+
+# Pulley samples:       shared samples with genes in the same direction 
+#
+#   ---S--- + ---G-------->
+#   ---S--- + ---G--->
+#   ---S--- + ---G------------>     ...only one of these samples are kept, rest are thrown...
+# 
+#   dealt with by making a dictionary that ensures a 1 to 1 key-value relationshipof sample-to-geneId
+#
+#   during the next .readline() loop utrcoord-to-geneid value will be checked against the current geneId in current iteration
+#
+geneIds = headers.keys()
+one_coord_per_geneId = {}
+for geneId in geneIds:
+    utrCoord = headers[geneId]['utrCoord']['chromosome']+':'+headers[geneId]['utrCoord']['start']+'-'+headers[geneId]['utrCoord']['end']+':'+headers[geneId]['utrCoord']['strand']
+    one_coord_per_geneId[utrCoord] = geneId   # To ensure only one geneId = one utrCoord, I purposefully allow values to be overidden for a given key 
+
+# Tug'o'war samples:    shared samples with genes in the opposite direction 
+#
+#   s1  <-------G--- + ---S---
+#   s2                 ---S--- + ---S------->
+#
+#   dealt with by making a set of unique utrcoords stripped of their strand property, now upon encountering a match forward samples and reverse samples take their half of the tug-o-war sequence
+#
+coords          = [ ':'.join(i.split(':')[0:-1]) for i in one_coord_per_geneId.keys() ]     # remove strand property
+coords_shared   = set([i for i in coords if coords.count(i)>1])
+
+print 'Generating meme-ready data...'
+
+file_in = open(file_in_name) # Open file for formatted parsing
+collection = []
+coords_shared_geneIds = []
+
+print ('\tFiltering shared samples...')
+
+while True:    
+    header  = file_in.readline()
+    sequence= file_in.readline().rstrip()
+    if header == "": # break when finished
+        break
+# MEME filter:  =>  Is this sample seq too short for meme? 
+    # YES:
+    if len(sequence)<10:
+        if sequence=='\n':
+            collection.append(header)
+        continue
+    # NO:
+    else:
+        geneId  = header.split('\t')[0].replace('>','')
+        utrCoord= header.split('\t')[1].replace('UtrCoord:','').split(':')
+    # PULLEY filter:            =>  Is this GeneId unique? or permitted as a winning pulley sample?
+        if one_coord_per_geneId[':'.join(utrCoord)] == geneId:
+            utrCoord_noStrand   = ':'.join(utrCoord[0:1+1])
+        # TUG'O'WAR filter:     =>  Is this sample in a tug'o'war? 
+            if utrCoord_noStrand in coords_shared:
+                utrCoord_onlyStrand = utrCoord[2]
+                coords_shared_geneIds.append(header.split('\t')[0].replace('>',''))
+                sequence = sequence[(len(sequence)/2):] # this works, since the rightermost end of the seq = gene, so we must sample from the end inwards
+            # OKAY:             => then upon passing the filters, we can write the sample :)
+            file_out.write(header+sequence+'\n')
+# DUSTMASKER: low complexity region masker
+print('\tMasking low complexity regions...')
+subprocess.call(['dust',file_out_name,'10'],stdout=file_out) # TODO: what value do I use for 'cut-off', here given as 10
+
+file_in.close()
+file_out.close()
+
+
+#================================================================================================
+# PULLEY Samples:       exactly shared sample seqs, such that                   SOLVED
+#================================================================================================
+#
+# s1    ---S--- + ---G------->
+# s2    ---S--- + ---G--->
+#
+#   ...notice the two forward facing genes share the sample upstream, 
+#   ...to prevent MEME from over-representing this we allocate it to just one of the samples
+#
+#
+#================================================================================================
+# OVERLAP sampels:      partially shared Sample Seqs                            UNSOLVED
+#================================================================================================
+#
+#  Can be a problem to try and distirbute out the overlapping sample_seqs <==...
+# 
+# Samples: 
+#  s1             ----------------------
+#  s2       --------------------
+#  s3                             -----------
+#  s4         -----------
+#  s5             ------------------
+# 
+#       ...well then how can we possibly distribute out this overlapping region between correponding samples? 
+#          ...there seems only one option: remove the intersect! => but then our data will be so sparse!
+#
+# After rm intersect:
+#
+#  s1        |   ----------------------|
+#  s2      --|------------------       |
+#  s3        |                    -----|------
+#  s4        |-----------              |
+#  s5        |   ------------------    |
+# 
+#           ^ Not much data left!!!     ^
+#
+#
+#================================================================================================
+# TUG'O'WAR Samples:    samples that are shared by opposite facing genes        SOLVED
+#================================================================================================
+#
 #
 # Reap all coordinates that are fully shared by two adjacent genes which are in opposite orientation, a.k.a "Tug-Of-War" orientation
 #
@@ -72,53 +157,9 @@ file_in.close()
 #   s2                 ...S---
 #
 #
-geneIds         = headers.keys()
-coords          = [  headers[i]['utrCoord']['chromosome']+':'+headers[i]['utrCoord']['start']+'-'+headers[i]['utrCoord']['end'] for i in geneIds ]
-coords_shared   = set([i for i in coords if coords.count(i)>1])
-
-file_in         = open(file_in_name)                   # Open file for reading again
-
-print 'reading file...'
-collection = []
-coords_shared_geneIds = []
-count = 0
-while True:
-    count = count + 1
-    header  = file_in.readline()
-    sequence= file_in.readline().rstrip()
-    if header == "":                    # break when finished
-        break
-    if len(sequence)<10:
-        if sequence=='\n':
-            collection.append(header)
-        continue
-    else:
-        utrCoord            = header.split('\t')[1].replace('UtrCoord:','').split(':')
-        utrCoord_noStrand   = ':'.join(utrCoord[0:1+1])
-
-        if utrCoord_noStrand in coords_shared:
-            utrCoord_onlyStrand = utrCoord[2]
-            coords_shared_geneIds.append(header.split('\t')[0].replace('>',''))
-            # ORIENTATION OPTION 1 :                                ----v   which of these options is correct? / Hint: think about the revcomp as seqs exit the db
-            sequence = sequence[(len(sequence)/2):] # this works, since the rightermost end of the seq = gene, so we must sample from the end in
-            # sequence = sequence[0:(len(sequence)/2)] # this fails
-
-            # ORIENTATION OPTION 2 :                                ----^
-            # if utrCoord_onlyStrand=='1':
-            #     print('plus')
-            #     sequence = sequence[(len(sequence)/2):]
-            # elif utrCoord_onlyStrand=='-1':
-            #     print('minus')
-            #     sequence = sequence[0:(len(sequence)/2)+1]
-
-        file_out.write(header+sequence+'\n')
-
-file_in.close()
-file_out.close()
 
 
-
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # ================================================================
 # OLD METHODS:
