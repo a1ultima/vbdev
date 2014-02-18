83aa7454e17d7f878a00e5f4d241998fc7521690
a1ultima <a1ultima@gmail.com>
2014-02-18 18:02:53 +0000

    -meme_params.py - V1 - run meme with parameterizations via subprocess

---
+++ 2kb/scripts/meme_params.py
@@ -0,0 +1,10 @@
+
+import subprocess
+
+
+#command = 'meme anopheles_gambiae_upstream_memeready_25.fasta -dna -mod anr -revcomp -minw 10 -maxw 12 -nmotifs 25 -maxsize 50000000 -p 4 -o ../out/ag'.split(' ')
+
+#command = 'meme anopheles_gambiae_upstream_memeready_25.fasta -dna -mod oops -revcomp -w 10 -nmotifs 1 -maxsize 50000000 -p 4 -o ../out/ag'.split(' ')
+
+subprocess.call(command)
+
