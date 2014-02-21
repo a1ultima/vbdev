2b5cd630f316e3594d5d126da84b68a41b76ee06
a1ultima <a1ultima@gmail.com>
2014-02-21 17:15:49 +0000

    bfileGenerator.pl - V1 - Bobs bfile generator for meme in perl

---
+++ 2kb/scripts/bfileGenerator.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -w
+
+#
+# usage: ./this-prog.pl -order 2 input.fasta > output.bg2
+#
+
+#
+# warning, doesn't seem to handle masked sequences at all??
+#
+
+use Getopt::Long;
+
+my $order = 3;
+my $skipids = '^CR';
+
+GetOptions("order=i"=>\$order,
+           "skipids=s"=>\$skipids,
+          );
+
+my %seqs;
+my $id;
+while (<>) {
+  if (/^>(\S+)/) {
+    $id = $1;
+    undef $id if ($id =~ /$skipids/);
+  } elsif ($id) {
+    s/\W//;
+    $seqs{$id} .= $_;
+  }
+}
+
+foreach my $k (1 .. $order) {
+  my %count;
+  my $n = 0;
+  foreach $id (keys %seqs) {
+    my $seq = $seqs{$id};
+    my $len = length($seq);
+    for (my $i=0; $i<$len-($k-1); $i++) {
+      $count{substr($seq, $i, $k)}++;
+      $n++;
+    }
+  }
+  my $expected = 4**$k;
+  if (keys %count < $expected) {
+    die "not all $k-tuplets seen in input seqs\n";
+  }
+  print "# order = $k\n";
+  foreach my $subseq (sort keys %count) {
+    printf "%-10s %f\n", lc($subseq), $count{$subseq}/$n;
+  }
+}
