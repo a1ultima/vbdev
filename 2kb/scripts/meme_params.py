d91c81549b9064c5faa5fc492b08b96159e47447
a1ultima <a1ultima@gmail.com>
2014-03-10 15:11:39 +0000

    meme_params.py - V1 - pipelineready

--- 2kb/scripts/meme_params.py
+++ 2kb/scripts/meme_params.py
@@ -1,32 +1,57 @@
-
 import subprocess
 from datetime import datetime
-
-#command = 'meme anopheles_gambiae_upstream_memeready_25.fasta -dna -mod anr -revcomp -minw 10 -maxw 12 -nmotifs 25 -maxsize 50000000 -p 4 -o ../out/ag'.split(' ')
-#meme = 'meme anopheles_gambiae_upstream_memeready_25.fasta -dna -mod oops -revcomp -w 10 -nmotifs 1 -maxsize 50000000 -p 4 -o ../out/ag'.split(' ')
-
 import os 
+
 home = os.path.expanduser('~')
 os.chdir(home)
 
-in_bfile = '0VB/2kb/data/meme_data/in/'     + 'anopheles_gambiae.bg2'
-in_fasta = '0VB/2kb/data/meme_data/test/'   + 'anopheles_gambiae_upstream_memeready_100_dusted_c5.fasta'
-out_meme = '0VB/2kb/data/meme_data/out/'    + 'ag_anr'
 
-meme_anr = [    'meme', 
-                in_fasta, 
-                '-dna', 
-                '-mod', 'anr', 
-                '-revcomp', 
-                '-w', '10', 
-                '-nmotifs', '10', 
-                '-maxsize', '50000000', 
-                '-p', '4', 
-                '-bfile', in_bfile,
-                '-oc', out_meme
-                ]
+program = 'dreme' 
 
-all_meme = [meme_anr]
+
+species  = 'Anopheles gambiae'.lower().replace(' ','_')
+
+
+in_fasta = '0VB/2kb/data/meme_data/test/'   + species + '_upstream_memeready_1000_dusted_c2.fasta'
+out_meme = '0VB/2kb/data/meme_data/out/'    + species + '_anr'
+
+
+if program == 'meme':
+    in_bfile = '0VB/2kb/data/meme_data/in/'     + species + '.bg2'
+    meme_anr = [    'meme',                     # MEME program
+                    in_fasta,                   # input fasta data , e.g. 2kb upstream vb & dust masked
+                    '-dna',                     # dna motifs
+                    '-revcomp',                 # + and - strands
+                    '-mod',     'anr',          # how many occurrences of that motif per sequence
+                    '-w',       '10',           # motif size to scan for 
+                    '-nmotifs', '10',           # no. motifs to output
+                    '-maxsize', '50000000',     # max inputfile size in characters
+                    '-evt',     '0.0001',        # if e-value of motif > -evt: skip this motif
+                    '-p',       '5',            # no. cores
+                    '-bfile',   in_bfile,       # background markov model, Bob's script generates these at order 2 
+                    '-oc',      out_meme
+                    ]
+    all_meme = [meme_anr]
+
+elif program=='dreme':
+
+    #in_bfile = '0VB/2kb/data/meme_data/in/'     + species + '.bg2'
+
+    meme_anr = [    'meme',                     # MEME program
+                    in_fasta,                   # input fasta data , e.g. 2kb upstream vb & dust masked
+                    '-dna',                     # dna motifs
+                    '-revcomp',                 # + and - strands
+                    '-mod',     'anr',          # how many occurrences of that motif per sequence
+                    '-w',       '10',           # motif size to scan for 
+                    '-nmotifs', '10',           # no. motifs to output
+                    '-maxsize', '50000000',     # max inputfile size in characters
+                    '-evt',     '0.0001',        # if e-value of motif > -evt: skip this motif
+                    '-p',       '5',            # no. cores
+                    '-bfile',   in_bfile,       # background markov model, Bob's script generates these at order 2 
+                    '-oc',      out_meme
+                    ]
+    all_meme = [meme_anr]
+
 
 for meme in all_meme:
     start   = subprocess.Popen(['date'], stdout=subprocess.PIPE).communicate()[0]
